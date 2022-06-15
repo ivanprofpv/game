346fdaae7d8fc7b1b512f2dd795f0fc92e195a73
Ivan <98175188+ivanprofpv@users.noreply.github.com>
2022-06-16 02:02:10 +0400

    Add files via upload

--- dealer.rb
+++ dealer.rb
@@ -1,7 +1,7 @@
 class Dealer < Player
 
   def choose
-    puts "У Дилера в руке: #{'* ' * hand.size} карты"
+    puts "У #{self.class} в руке: #{'* ' * hand.size} карты"
     points < 17 ? 2 : 1
   end
 
